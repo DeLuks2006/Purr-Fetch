22c001adf0e4c00a6ca03e3a31ce1f4bfd3574cd
Ihor Tymkiv <102431570+glitchingcore@users.noreply.github.com>
2023-07-29 18:39:37 +0200

    Create fetch.cpp

---
+++ fetch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+double getUptime();
+std::string getShell();
+std::string getDistro();
+std::string getHostname();
+std::string getKernel();
+void display();
+
+int main()
+{
+	return 0;
+}
+
