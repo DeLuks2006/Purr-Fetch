0d01ce3d50243b273668809043c6621764603ed7
Ihor Tymkiv <102431570+glitchingcore@users.noreply.github.com>
2023-07-29 19:16:09 +0200

    Implement getUptime()

--- fetch.cpp
+++ fetch.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 double getUptime();
+// Returns uptime in seconds
 std::string getShell();
 std::string getDistro();
 std::string getHostname();
@@ -13,3 +15,12 @@ int main()
 	return 0;
 }
 
+double getUptime()
+{
+	double uptimeSeconds;
+	if (std::ifstream("/proc/uptime") >> uptimeSeconds)
+	{
+		return uptimeSeconds;
+	}
+	return -1;
+}
