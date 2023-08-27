053efac2278494b5b464e6b84d4528e7f1004f7a
DeLuks <87036369+DeLuks2006@users.noreply.github.com>
2023-08-27 12:55:52 +0200

    Add files via upload

--- fetch.c
+++ fetch.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/utsname.h>
+#include <string.h>
 
 // hostname 4 - read file /etc/hostname
 // kernel 5 - read file or osInfo.sysname 
 // ascii art 0
 
-#define reset \x1b[0;0m
 
+void display();
 const char* uptime();
 const char* shell();
 const char* distro();
+const char* hostname();
+const char* kernel();
 
 int main(){
-  printf("uptime: %s\nshell: %s\ndistro: %s", uptime(), shell(), distro());
+  display();
   return 0;
 }
 const char* shell(){
-  const char shell = getenv("SHELL"); // ignore this, idk how to fetch the shell
+  const char* shell = getenv("SHELL"); 
   return shell;
 }
 
@@ -39,34 +43,39 @@ const char* distro(){
   static char sDistro[100];
   FILE *fptr = fopen("/etc/issue", "r");
 
-  if (fptr == NULL)
+  if (fptr == NULL){
     printf("Error opening file :(\n");
     exit(1);
-  
+  }
   fscanf(fptr, "%s",sDistro);
   fclose(fptr);
   return sDistro;
 }
 const char* hostname(){
-  static char sHost[50] = ;
-  int status = gethostname(sHost, sizeof(hostname));
-  if (status == 0)
+  static char sHost[50];
+  int status = gethostname(sHost, sizeof(sHost));
+  if (status == 0){
     return sHost;
-
+  }
   return "";
 }
 
 const char* kernel(){
-  // TODO: gotta learn structs and make this
+  struct utsname buffer;
+  if (uname(&buffer) == 0){
+    return buffer.release;
+  } else {
+    return "";
+  }
 }
 
 
 
-/*void display(){
-  printf("(\\_/)");// uptime  - orange
-  printf("(oᴥo)"); // shell   - red
-  printf("|U°U|"); // distro  - purple
-  printf("|   |"); // host    - blue
-  printf("'U_U'"); // kernel  - cyan
-  printf("  U");
-}*/
+void display(){
+  printf("(\\_/)\t\033[0;33m uptime: %s\n\033[0;0m", uptime());// uptime  - orange
+  printf("(oᴥo)\t\033[0;31m shell: %s\n\033[0;0m", shell()); // shell   - red
+  printf("|U°U|\t\033[0;35m distro: %s\n\033[0;0m", distro()); // distro  - purple
+  printf("|   |\t\033[0;34m hostname: %s\n\033[0;0m", hostname()); // host    - blue
+  printf("'U_U'\t\033[0;36m kernel: %s\n\033[0;0m", kernel()); // kernel  - cyan
+  printf("  U\n");
+}
